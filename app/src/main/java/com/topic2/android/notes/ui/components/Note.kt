5745370e82b910aa4f7a18d48617850771dfd886
Vadim <zyuskovv@gmail.com>
2024-02-08 19:51:52 +0300

    Note created

---
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -0,0 +1,12 @@
+package com.topic2.android.notes.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable fun Note(){
+
+}
+@Preview
+@Composable
+private fun NotePreview(){ Note()
+}
